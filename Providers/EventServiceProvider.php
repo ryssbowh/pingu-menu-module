cf0561bef1b91f6d2d6f3263c1b486fd7a2aad2e
Boris Blondin <blondin.boris@gmail.com>
2019-12-07 09:47:38 +0000

    fix permission cache

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -5,6 +5,7 @@ namespace Pingu\Menu\Providers;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Pingu\Menu\Events\MenuCacheChanged;
 use Pingu\Menu\Events\MenuItemCacheChanged;
+use Pingu\Menu\Listeners\EmptyMenuBuiltCache;
 use Pingu\Menu\Listeners\EmptyMenuCache;
 use Pingu\Permissions\Events\PermissionCacheChanged;
 
@@ -18,7 +19,7 @@ class EventServiceProvider extends ServiceProvider
             EmptyMenuCache::class
         ],
         PermissionCacheChanged::class => [
-            EmptyMenuCache::class
+            EmptyMenuBuiltCache::class
         ]
     ];
 }
